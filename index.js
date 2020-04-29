9f9f3948eaad6aa14af6b61f0d891e0f46f94b84
douglas541 <64561748+douglas541@users.noreply.github.com>
2020-04-29 20:08:54 -0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function getRandomNumber(){
-    var dig = []
-    for(var i = 0 ; i < 3 ; i++){
-        dig[i] = Math.floor(Math.random() * 255)
-    }
-    return `rgb(${dig[0]}, ${dig[1]}, ${dig[2]})`
-}
-
-document.body.style.backgroundColor = getRandomNumber()
